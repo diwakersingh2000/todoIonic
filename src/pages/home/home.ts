825254430e1a0fbd93b7b884120006c759c719f1
Diwaker Singh <diwaker@decorastudio.com>
2018-02-09 18:43:29 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public alertCtrl: AlertController,
+              public navCtrl: NavController) {}
+  
+  downloadModel = function(){
+    let link = this.downloadLink;
+    if (link.includes("vizmonk")){
+      console.log("valid link");
+    }
+    else{
+        let alert = this.alertCtrl.create({
+          title: 'Oops!',
+          subTitle: 'Invalid Link!',
+          buttons: ['OK']
+        });
+        alert.present();
+    }
   }
 
 }
